3b2ec1b60da2f5de1d4543504a6027b7677b9e20
6Morpheus6 <wanton-decay@gmx.net>
2025-11-18 21:05:02 +0100

    update
    - move hf-xet, pydantic to requirements.txt

--- install.js
+++ install.js
@@ -17,8 +17,7 @@ module.exports = {
         venv: "env",
         path: "app",
         message: [
-          "uv pip install -r ../requirements.txt",
-          "uv pip install pydantic==2.10.6 hf-xet"
+          "uv pip install -r ../requirements.txt"
         ]
       }
     },
